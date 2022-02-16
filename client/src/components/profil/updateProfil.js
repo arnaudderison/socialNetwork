70ba37d827b7ed5478b598119b69ea7c82670c6b
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-02-16 15:57:28 +0100

    tr

--- client/src/components/profil/updateProfil.js
+++ client/src/components/profil/updateProfil.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function UpdateProfil() {
+    const userData = useSelector((state) => state.userReducer)
     return (
-        <div>
-            UPDATE 
+        <div className='container-profil'>
+            <div className='card-box'>
+                <div className='title-card'>
+                    <h1>{userData.pseudo}</h1>
+                </div>
+                <div className='update-container'>
+                    <img src={`images/ ${userData.picture}`}/>
+                </div>
+            </div>
         </div>
     )
 }
